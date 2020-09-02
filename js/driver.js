fcf39c37788bcdbf00d2d31e86f2e08a3fb76a65
vald0007 <40250182+vald0007@users.noreply.github.com>
2020-09-02 10:46:54 -0500

    Added javascript update for intro icons

--- js/driver.js
+++ js/driver.js
@@ -1,5 +1,15 @@
-var dropdown = document.getElementsByClassName("collapse");
-
-for(var i = 0; i < dropdown.length; i++){
-
+function showContent(id){
+  console.log("Here");
+  var expandible = document.getElementById(id);
+  var container = document.getElementById("intro-container");
+  
+  if (expandible.style.display === "none" || expandible.style.display === '') {
+    console.log("Here");
+    expandible.style.display = "block";
+    container.style.paddingBottom = "11%";
+  } 
+  else {
+    console.log("Here but different");
+    expandible.style.display = "none";
+  } 
 }
