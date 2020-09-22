ae7dbc2579d910df3231216d2ea28ee7588fc0f1
vald0007 <40250182+vald0007@users.noreply.github.com>
2020-09-22 10:53:26 -0500

    Fixed content and removed js

--- js/driver.js
+++ js/driver.js
@@ -1,16 +1,13 @@
 function showContent(id){
-  console.log("Here");
   var expandible = document.getElementById(id);
   var container = document.getElementById("intro-container");
   
   if (expandible.style.display === "none" || expandible.style.display === '') {
-    console.log("Here");
     expandible.style.display = "block";
     container.style.paddingBottom = "22%";
 
   } 
   else {
-    console.log("Here but different");
     expandible.style.display = "none";
   } 
 }
